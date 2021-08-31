f8617a3599a2e8235d1c3179631682cad1b1a582
Jordan Smithson <Jordan Smithson>
2021-08-31 15:04:01 -0600

    set up initial header

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,20 @@ import routes from '../Routes'
 import { Router } from 'react-router-dom'
 import history from '../History'
 
+import Header from '../components/Header'
+
 const App = () => {
     return (
-        <Router history = {history} >
-            <div>
-                {routes}
-            </div>
-        </Router>
+        <div className = 'ui container' >
+            <Router history = {history} >
+                <Header />
+                <div>
+                    {routes}
+                </div>
+            </Router>
+        </div>
     )
 }
+
 
 export default App
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const Header = () => {
+    return (
+        <div className = 'ui secondary menu'>
+            <h4 className = 'item'>Recipe App</h4>
+            <div className = 'right menu'>
+                <button className = 'ui negative'> Google</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
